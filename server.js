aa86ecd4ac765e3d65df8af74e9f64d3dbb3b862
kuznetsovandrey76 <and.rey.q@yandex.ru>
2017-02-15 21:34:41 +0300

    init

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var randomColor = require('./lib/color.js');
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+var handlebars  = require('express-handlebars')
+	.create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+	res.render('home');
+});
+
+app.get('/about', function(req, res) {
+	res.render('about', {color: randomColor.getColor()});
+});
+
+app.use(function(req, res, next) {
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+
+app.listen(app.get('port'), function() {
+	console.log('Express запущен на http://localhost:' + 
+		app.get('port') + ': нажмите CTRL+C для завершения');
+});
+
